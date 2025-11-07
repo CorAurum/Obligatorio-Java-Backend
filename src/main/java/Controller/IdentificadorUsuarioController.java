b3f7921b008d56b33d0b4a4f51a09f8ff5c047a6
Fabricio <Fabriciogarcia20@gmail.com>
2025-11-06 22:59:42 -0300

    hotfix

--- src/main/java/Controller/IdentificadorUsuarioController.java
+++ src/main/java/Controller/IdentificadorUsuarioController.java
@@ -7,7 +7,7 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import java.util.List;
 
-@Path("/api/identificadores")
+@Path("/identificadores")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class IdentificadorUsuarioController {
